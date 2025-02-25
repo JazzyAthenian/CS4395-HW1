a87b9ab42cb8764c89be13bb83c9b68005e352e3
Jasmine Mathers <39599390+JazzyAthenian@users.noreply.github.com>
2025-02-25 17:57:13 -0600

    updated preprocessing function

--- bigram.py
+++ bigram.py
@@ -1,19 +1,23 @@
 import re
+from nltk.stem import WordNetLemmatizer
 
 def preprocessing(text):
     text = text.lower()
     text = re.sub('[^a-z0-9\.\-]', ' ', text) # remove special characters
     text = re.sub('\s{2,}', ' ', text) # change multiple spaces to 1 space
     text = '. ' + text
-    return text
+
+    wnl = WordNetLemmatizer()
+    tokens = text.split()
+    tokens = [wnl.lemmatize(token) for token in tokens]
+    return tokens
 def compute_bigram(fileName):
     f = open(fileName, "r")
     bigrams = dict()
 
     # going through training data one review at a time
     for line in f:
-        text = preprocessing(line)
-        tokens = text.split()
+        tokens = preprocessing(line)
 
         # filling out dictionary
         for i in range(len(tokens)-1):
@@ -33,6 +37,8 @@ def compute_bigram(fileName):
         bigrams[word] = dict(sorted(bigrams[word].items(), key=lambda item: -item[1]))
         total = sum(bigrams[word].values())
         bigrams[word] = dict([(k, bigrams[word][k]/total) for k in bigrams[word]])
+
+    f.close()
 
 if __name__ == '__main__':
     compute_bigram("train.txt")
