1c5e0567e4f2b6f006af612644ec3d9c3821942a
Jasmine Mathers <39599390+JazzyAthenian@users.noreply.github.com>
2025-02-25 17:56:03 -0600

    Added Lemmatization

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,11 +1,17 @@
 import re
+from nltk.stem import WordNetLemmatizer
 
 def preprocessing(text):
     text = text.lower()
     text = re.sub('[^a-z0-9\.\-]', ' ', text) # remove special characters
     text = re.sub('\s{2,}', ' ', text) # change multiple spaces to 1 space
     text = '. ' + text
-    return text
+
+    wnl = WordNetLemmatizer()
+    tokens = text.split()
+    tokens = [wnl.lemmatize(token) for token in tokens]
+    return tokens
+
 
 if __name__ == '__main__':
     f = open("train.txt", "r")
